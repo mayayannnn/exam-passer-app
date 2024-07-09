d84fdbf90653548a960afb831a3bc996b413caa3
mayayann <s.mayayann.2010@gmail.com>
2024-07-09 20:57:37 +0900

    add_many_page

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def select_main_category():
     return render_template("select_main_category.html")
 
+@app.route("/select-sub-category")
+def select_sub_category():
+    return render_template("select_sub_category.html")
+
+@app.route("/quiz/<id>")
+def quiz(id):
+    return render_template("quiz.html")
+
+@app.route("/answer/<id>")
+def answer(id):
+    return render_template("answer.html")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
 if __name__ == "__main__":
     app.run(debug=True)

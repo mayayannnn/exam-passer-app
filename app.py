249f8aa8d62282b0984b4b42a23ad6e636ef7eac
mayayann <s.mayayann.2010@gmail.com>
2024-07-09 21:34:39 +0900

    add_databese

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from database import Category
 from flask import Flask
 from flask import render_template
 
---
+++ database.py
@@ -0,0 +1,26 @@
+from peewee import *
+
+db = SqliteDatabase("exam_passer_app.db")
+
+class MainCategory(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class SubCategory(Model):
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class Category(Model):
+    main_category_id = IntegerField()
+    sub_category_id = IntegerField()
+
+    class Meta:
+        database = db
+
+
+
+db.create_tables([MainCategory,SubCategory,Category])
